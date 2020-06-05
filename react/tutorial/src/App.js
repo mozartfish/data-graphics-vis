b9dc1d2fe7b8f2b8fa46a670d4fc59bb7d80136a
Pranav Rajan <rajan.pranav320@gmail.com>
2020-06-04 23:02:32 -0700

    set up the to do list

--- react/tutorial/src/App.js
+++ react/tutorial/src/App.js
@@ -2,20 +2,37 @@ import React from "react";
 // import Footer from "./components/Footer.jsx";
 // import MainContent from "./components/MainContent.jsx";
 // import Header from "./components/Header.jsx";
+import ToDoItem from "./components/ToDoItem.jsx";
 function App() {
+  // const date = new Date()
+  // const hours = date.getHours()
+  // let timeOfDay;
+  // if (hours < 12) {
+  //     timeOfDay = "morning"
+  // } else if (hours >= 12 && hours < 17) {
+  //     timeOfDay = "afternoon"
+  // } else {
+  //     timeOfDay = "night"
+  // }
+  // const styles = {
+  //     color: "green",
+  //     backgroundColor: "Orange",
+  //     fontSize: "200px"
+  // }
+  // return (
+  //     // inline styling with react
+  // <h1 style={styles}>Good {timeOfDay}!</h1>
+  // )
+  // const firstName = "Pranav"
+  // const lastName = "Rajan"
+  // return (
+  // <h1>Hello {`${firstName} ${lastName}`}!</h1>)
   return (
     <div>
       {/* <Header />
       <MainContent />
       <Footer /> */}
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
-      <input type="checkbox" />
-      <p>Placeholder text here</p>
+      <ToDoItem />
     </div>
   );
 }
---
+++ react/tutorial/src/components/ToDoItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+function ToDoItem() {
+    return (
+        <div>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+        <input type="checkbox" />
+        <p>Placeholder text here</p>
+      </div>
+    )
+}
+export default ToDoItem;
