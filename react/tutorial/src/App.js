197f5397b1c298fc6784753b6e0b13986340d87d
Pranav Rajan <rajan.pranav320@gmail.com>
2020-06-06 17:28:08 -0700

    got props stuff set up

--- react/tutorial/src/App.js
+++ react/tutorial/src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 // import Footer from "./components/Footer.jsx";
 // import MainContent from "./components/MainContent.jsx";
 // import Header from "./components/Header.jsx";
-import ToDoItem from "./components/ToDoItem.jsx";
+// import ToDoItem from "./components/ToDoItem.jsx";
+import ContactCard from "./components/ContactCard.jsx";
 function App() {
   // const date = new Date()
   // const hours = date.getHours()
@@ -27,12 +28,40 @@ function App() {
   // const lastName = "Rajan"
   // return (
   // <h1>Hello {`${firstName} ${lastName}`}!</h1>)
+  //   return (
+  //     <div>
+  //       {/* <Header />
+  //       <MainContent />
+  //       <Footer /> */}
+  //       {/* <ToDoItem /> */}
+  //     </div>
+  //   );
   return (
-    <div>
-      {/* <Header />
-      <MainContent />
-      <Footer /> */}
-      <ToDoItem />
+    <div className="contacts">
+      <ContactCard
+        name="Mr. Whiskerson"
+        imgUrl="http://placekitten.com/300/200"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <ContactCard
+        name="Mr.C"
+        imgUrl="http://placekitten.com/400/200"
+        phone="(212) 555-3456"
+        email="mr.c@catnap.meow"
+      />
+      <ContactCard
+        name="Mr. The Cat"
+        imgUrl="http://placekitten.com/400/300"
+        phone="(212) 555-7829"
+        email="mr.thecat@catnap.meow"
+      />
+      <ContactCard
+        name="Mr. Furry Friend"
+        imgUrl="http://placekitten.com/200/100"
+        phone="(212) 555-1065"
+        email="mr.furryFriend@catnap.meow"
+      />
     </div>
   );
 }
