2fbfa0d91557534b8dd9c94825c46c1b5f80c4c0
Pranav Rajan <rajan.pranav320@gmail.com>
2020-06-29 14:54:38 -0700

    'alternating colors on clicks'

--- vis/d3Selections/js/main.js
+++ vis/d3Selections/js/main.js
@@ -1,5 +1,18 @@
-d3.selectAll("circle")
-  .style("fill", "orange")
-  .attr("r", function () {
-    return 10 + Math.random() * 40;
-  });
+// randomly select circles and update them
+// d3.selectAll("circle")
+//   .style("fill", "orange")
+//   .attr("r", function () {
+//     return 10 + Math.random() * 40;
+//   });
+
+// select circles and update their colors
+let counter = 0;
+d3.selectAll("circle").on("click", function (d, i) {
+  if (counter == 0) {
+    d3.selectAll("circle").style("fill", "red");
+    counter++;
+  } else {
+    d3.selectAll("circle").style("fill", "cyan");
+    counter--;
+  }
+});
