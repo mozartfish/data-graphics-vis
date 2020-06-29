e71a8b63e6df3287503c29a837975973d2413636
Pranav Rajan <rajan.pranav320@gmail.com>
2020-06-29 15:00:14 -0700

    'modified colors and added individual click update'

--- vis/d3Selections/js/main.js
+++ vis/d3Selections/js/main.js
@@ -6,13 +6,18 @@
 //   });
 
 // select circles and update their colors
-let counter = 0;
+// let counter = 0;
+// d3.selectAll("circle").on("click", function (d, i) {
+//   if (counter == 0) {
+//     d3.selectAll("circle").style("fill", "red");
+//     counter++;
+//   } else {
+//     d3.selectAll("circle").style("fill", "cyan");
+//     counter--;
+//   }
+// });
+
+// modifying individual circles
 d3.selectAll("circle").on("click", function (d, i) {
-  if (counter == 0) {
-    d3.selectAll("circle").style("fill", "red");
-    counter++;
-  } else {
-    d3.selectAll("circle").style("fill", "cyan");
-    counter--;
-  }
+  d3.select(this).style("fill", "cyan");
 });
