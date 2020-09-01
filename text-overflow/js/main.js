e02eddc41fb4662967566cb9b33192f60e922a8f
Pranav Rajan <rajan.pranav320@gmail.com>
2020-08-31 23:11:09 -0700

    finally managed to draw text correctly

---
+++ text-overflow/js/main.js
@@ -0,0 +1,27 @@
+// create svg
+let svg = d3.select("body").append("svg");
+svg.attr("width", 100).attr("height", 200).attr("id", "hello-svg");
+
+// set up the data for the svg
+let textData = [
+  "Floccinaucinihilipilification",
+  "Floccinaucinihilipilification",
+];
+
+// select all the stuff
+let telements = d3
+  .select("#hello-svg")
+  .selectAll("text")
+  .data(textData)
+  .enter()
+  .append("text");
+
+telements
+  .attr("x", 5)
+  .attr("y", (d, i) => {
+    return i * 20 + 60;
+  })
+  .attr("width", 150)
+  .attr("height", 30)
+  .classed("text-info", true)
+  .text((d) => d);
