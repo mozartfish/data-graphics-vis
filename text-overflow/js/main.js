17de37220785c4601dca9d57420fdb92789e408d
Pranav Rajan <rajan.pranav320@gmail.com>
2020-09-01 08:56:54 -0700

    "got the foreign object appended to the screen"

--- text-overflow/js/main.js
+++ text-overflow/js/main.js
@@ -1,6 +1,6 @@
 // create svg
 let svg = d3.select("body").append("svg");
-svg.attr("width", 100).attr("height", 200).attr("id", "hello-svg");
+svg.attr("width", 125).attr("height", 200).attr("id", "hello-svg");
 
 // set up the data for the svg
 let textData = [
@@ -9,19 +9,27 @@ let textData = [
 ];
 
 // select all the stuff
-let telements = d3
+// let telements = d3
+//   .select("#hello-svg")
+//   .selectAll("text")
+//   .data(textData)
+//   .enter()
+//   .append("text");
+
+// telements
+//   .attr("x", 5)
+//   .attr("y", (d, i) => {
+//     return i * 20 + 60;
+//   })
+//   .attr("width", 150)
+//   .attr("height", 30)
+//   .classed("text-info", true)
+//   .text((d) => d);
+
+// foreign object selection stuff
+let telememnts = d3
   .select("#hello-svg")
-  .selectAll("text")
+  .selectAll("foreignObject")
   .data(textData)
   .enter()
-  .append("text");
-
-telements
-  .attr("x", 5)
-  .attr("y", (d, i) => {
-    return i * 20 + 60;
-  })
-  .attr("width", 150)
-  .attr("height", 30)
-  .classed("text-info", true)
-  .text((d) => d);
+  .append("foreignObject");
